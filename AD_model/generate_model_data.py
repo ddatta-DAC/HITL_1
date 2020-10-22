5f295eee0c6550becbbe69006c3483b143e3dca2
Debanjan Datta <ddatta@vt.edu>
2020-10-22 14:04:52 -0400

    update

--- AD_model/generate_model_data.py
+++ AD_model/generate_model_data.py
@@ -16,6 +16,9 @@ parser.add_argument(
 
 args = parser.parse_args()
 DIR = args.DIR
+
+
+
 save_dir_stage_1 = './../generated_data_v1/us_import1'
 save_dir_stage_2 = './../generated_data_v1/us_import1/stage_2'
 
@@ -29,6 +32,12 @@ test_df.to_csv(os.path.join(save_dir_stage_2,'test_data_serialID.csv'),index=Non
 # =============================
 # Generate negative data
 # =============================
+# Drop duplicates
+ID_COL = 'PanjivaRecordID'
+attributes = list(train_df.columns)
+attributes.remove(ID_COL)
+train_df = train_df.drop_duplicates(subset=attributes)
+print('Post dropping duplicates, size of train set', len(train_df))
 x_pos, x_neg = utils.generate_negative_samples(train_df, DIR, num_neg_samples=10)
 # Save training data
 np.save(os.path.join(save_dir_stage_2,'train_x_pos.npy'),x_pos)
