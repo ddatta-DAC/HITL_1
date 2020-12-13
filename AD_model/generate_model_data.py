4bf5d4221b139bf9b11ac04dda085fe6f8ef7328
ddatta <ddatta@vt.edu>
2020-12-12 23:06:18 -0500

    update

--- AD_model/generate_model_data.py
+++ AD_model/generate_model_data.py
@@ -1,16 +1,24 @@
+# Script to generate data for AD model
+# -------------------------------------------
+# Anomaly Data is already serialized
+# Anomalies location: generated_data_v1/generated_anomalies/__DIR__
+# Positive : pos_anomalies.csv
+# Negative : neg_anomalies.csv
+
 import argparse
 import os
 import sys
-sys.path.append('./../..')
-sys.path.append('./..')
+sys.path.append('../../..')
+sys.path.append('../..')
 import pandas as pd
 try:
     import utils
 except:
     from common_utils import utils
 import numpy as np
-
+from pathlib import Path
 # ============================
+ID_COL = 'PanjivaRecordID'
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -20,55 +28,29 @@ parser.add_argument(
 
 args = parser.parse_args()
 DIR = args.DIR
-
-save_dir_stage_1 = './../generated_data_v1/{}'.format(DIR)
+data_source_dir = './../generated_data_v1/{}'.format(DIR)
+anomaly_source_dir = './../generated_data_v1/generated_anomalies/{}'.format(DIR)
 save_dir_stage_2 = './../generated_data_v1/{}/stage_2'.format(DIR)
+path = Path(save_dir_stage_2)
+path.mkdir(exist_ok=True,parents=True)
 
-# -------------------------------------
 
-test_normal_df = pd.read_csv(os.path.join(save_dir_stage_2, 'test_normal_data.csv' ), index_col=None)
-test_normal_df = utils.convert_to_serializedID_format(test_normal_df,DIR)
-test_normal_df.to_csv(os.path.join(save_dir_stage_2,'test_normal_serialized.csv'),index=None)
 
-test_neg_df = pd.read_csv(os.path.join(save_dir_stage_2, 'test_neg_data.csv' ), index_col=None)
-test_neg_df = utils.convert_to_serializedID_format(test_neg_df, DIR)
-test_neg_df.to_csv(os.path.join(save_dir_stage_2,'test_neg_serialized.csv'),index=None)
-
-test_pos_df = pd.read_csv(os.path.join(save_dir_stage_2, 'test_pos_data.csv' ), index_col=None)
-test_pos_df = utils.convert_to_serializedID_format(test_pos_df,DIR)
-test_pos_df.to_csv(os.path.join(save_dir_stage_2,'test_pos_serialized.csv'), index=None)
-
-train_df = pd.read_csv(os.path.join(save_dir_stage_1, 'train_data.csv' ), index_col=None)
-ID_COL = 'PanjivaRecordID'
+train_df = pd.read_csv(os.path.join(data_source_dir, 'train_data.csv' ), index_col=None)
 attributes = list(train_df.columns)
 attributes.remove(ID_COL)
 train_df = train_df.drop_duplicates(subset=attributes)
-print('Post dropping duplicates, size of train set', len(train_df))
-
-test_df = pd.read_csv(os.path.join(save_dir_stage_1, 'test_data.csv'), index_col=None)
 train_df = utils.convert_to_serializedID_format(train_df,DIR)
-test_df = utils.convert_to_serializedID_format(test_df, DIR)
-# Save the train data
-train_df.to_csv(os.path.join(save_dir_stage_2,'train_data_serialized.csv'), index=None)
-# ---------------------
-# Save test data
-# ---------------------
-test_df.to_csv(os.path.join(save_dir_stage_2,'test_data_serialID.csv'),index=None)
-
-# =============================
-# Generate negative data
-# =============================
-# Drop duplicates
-ID_COL = 'PanjivaRecordID'
-attributes = list(train_df.columns)
-attributes.remove(ID_COL)
-train_df = train_df.drop_duplicates(subset=attributes)
+train_df.to_csv(os.path.join(save_dir_stage_2,'train_serialized.csv'), index=None)
 print('Post dropping duplicates, size of train set', len(train_df))
-
 x_pos, x_neg = utils.generate_negative_samples(train_df, DIR, num_neg_samples=10)
 # Save training data
 np.save(os.path.join(save_dir_stage_2,'train_x_pos.npy'),x_pos)
 np.save(os.path.join(save_dir_stage_2,'train_x_neg.npy'),x_neg)
 
-
-
+# --------------------------------------
+test_normal_df = pd.read_csv(os.path.join(data_source_dir, 'test_data.csv' ), index_col=None)
+test_normal_df = utils.convert_to_serializedID_format(test_normal_df,DIR)
+test_normal_df = test_normal_df.drop_duplicates(subset=attributes)
+print('Post dropping duplicates, size of test set', len(test_normal_df))
+test_normal_df.to_csv(os.path.join(save_dir_stage_2,'test_serialized.csv'),index=None)
