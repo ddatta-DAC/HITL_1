5f6f50dab99e1c35c5be2443954bd6b3d6b2f125
Debanjan Datta <ddatta@vt.edu>
2021-02-05 11:55:03 -0500

    update

--- AD_model/exec.sh
+++ AD_model/exec.sh
@@ -1,5 +1,5 @@
-# python3 generate_model_data.py --DIR us_import1
-# python3 exec_AD_model.py --DIR us_import1 --saved_model model_11136_1603739417
+python3 generate_model_data.py --DIR us_import1
+python3 generate_model_data.py --DIR us_import2
 python3 generate_model_data.py --DIR us_import3
+python3 generate_model_data.py --DIR us_import4
 python3 generate_model_data.py --DIR us_import5
-python3 generate_model_data.py --DIR us_import6
