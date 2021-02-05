a3c1b0050c250d152a62e71c255822b3363a8a3c
ddatta <ddatta@vt.edu>
2021-02-05 14:51:11 -0500

    update

--- AD_model/ApE/exec.sh
+++ AD_model/ApE/exec.sh
@@ -1,15 +1,15 @@
-python3 exec_APE_model.py --DIR us_import1 --emb_dim 12 --lr 0.0005
-python3 exec_APE_model.py --DIR us_import1 --emb_dim 16 --lr 0.0005
+python3 train_models.py --DIR us_import1 --emb_dim 12 --lr 0.0005
+python3 train_models.py --DIR us_import1 --emb_dim 16 --lr 0.0005
 
-python3 exec_APE_model.py --DIR us_import2 --emb_dim 12 --lr 0.0005
-python3 exec_APE_model.py --DIR us_import2 --emb_dim 16 --lr 0.0005
-
-python3 exec_APE_model.py --DIR us_import3 --emb_dim 12 --lr 0.0005
-python3 exec_APE_model.py --DIR us_import3 --emb_dim 16 --lr 0.0005
-
-python3 exec_APE_model.py --DIR us_import4 --emb_dim 12 --lr 0.0005
-python3 exec_APE_model.py --DIR us_import4 --emb_dim 16 --lr 0.0005
-
-python3 exec_APE_model.py --DIR us_import5 --emb_dim 12 --lr 0.0005
-python3 exec_APE_model.py --DIR us_import5 --emb_dim 16 --lr 0.0005
+#python3 train_models.py --DIR us_import2 --emb_dim 12 --lr 0.0005
+#python3 train_models.py --DIR us_import2 --emb_dim 16 --lr 0.0005
+#
+#python3 train_models.py --DIR us_import3 --emb_dim 12 --lr 0.0005
+#python3 train_models.py --DIR us_import3 --emb_dim 16 --lr 0.0005
+#
+#python3 train_models.py --DIR us_import4 --emb_dim 12 --lr 0.0005
+#python3 train_models.py --DIR us_import4 --emb_dim 16 --lr 0.0005
+#
+#python3 train_models.py --DIR us_import5 --emb_dim 12 --lr 0.0005
+#python3 train_models.py --DIR us_import5 --emb_dim 16 --lr 0.0005
 
